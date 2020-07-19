e85033994aa83767c10c137a2c95f8f27c4ca3dc
Alejandro <alejandro@agtrace.ag>
2020-07-19 16:35:42 -0300

    Actualizando migraciones

--- erp/models/nomencladores/potencia.py
+++ erp/models/nomencladores/potencia.py
@@ -150,7 +150,7 @@ class Pontencia(models.Model):
                 if int(new_id) == int(res_id):
                     res_id = model.res_id
 
-                    # raise except_orm("AA" + str(res_id))
+                    raise except_orm("AA" + str(res_id))
 
 
 
