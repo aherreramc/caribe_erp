eb007d11d4961cdd1707bfe603c5cca06438cae7
Alejandro <alejandro@agtrace.ag>
2020-08-03 11:32:50 -0300

    Reporte product label

--- erp/report_models/nomencladores/product_label_report.py
+++ erp/report_models/nomencladores/product_label_report.py
@@ -46,9 +46,17 @@ class ProductLabelReport(models.AbstractModel):
     def _get_report_values(self, docids, data=None):
         report_obj = self.env['ir.actions.report']
         report = report_obj._get_report_from_name('module.report_name')
+
+        product = self.env['product.template'].browse(self.env.context.get('active_id'))
+
+        raise except_orm(product.name)
+
+
         docargs = {
             'doc_ids': docids,
             'doc_model': report.model,
             'docs': self,
+            'product': product,
+
         }
         return docargs
