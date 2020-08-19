479913644fe9aef43b4dd45c592c7586fc2bf8eb
Alejandro <alejandro@agtrace.ag>
2020-08-19 19:49:35 -0300

    cambiando name en sale.order.line

--- erp/models/operaciones/sale_order_line.py
+++ erp/models/operaciones/sale_order_line.py
@@ -8,24 +8,24 @@ import odoo.addons.decimal_precision as dp
 from openerp.exceptions import except_orm, Warning, RedirectWarning
 
 
-class SaleOrderLineTemplate(models.Model):
-    _inherit = 'sale.order.line'
-
-    # def name_get(self):
-    #     result = []
-    #     for so_line in self.sudo():
-    #         name = '%s - %s' % (so_line.order_id.name, so_line.name and so_line.name.split('\n')[0] or so_line.product_id.name)
-    #         if so_line.order_partner_id.ref:
-    #             name = '%s (%s)' % (name, so_line.order_partner_id.ref)
-    #         result.append((so_line.id, name))
-    #     return result
-
-
-    def name_get(self):
-        result = []
-        for so_line in self.sudo():
-            name = '%s - %s' % (so_line.product_id.descripcion_cliente)
-            # if so_line.order_partner_id.ref:
-            #     name = '%s (%s)' % (name, so_line.order_partner_id.ref)
-            result.append((so_line.id, name))
-        return result
+# class SaleOrderLineTemplate(models.Model):
+#     _inherit = 'sale.order.line'
+#
+#     # def name_get(self):
+#     #     result = []
+#     #     for so_line in self.sudo():
+#     #         name = '%s - %s' % (so_line.order_id.name, so_line.name and so_line.name.split('\n')[0] or so_line.product_id.name)
+#     #         if so_line.order_partner_id.ref:
+#     #             name = '%s (%s)' % (name, so_line.order_partner_id.ref)
+#     #         result.append((so_line.id, name))
+#     #     return result
+#
+#
+#     def name_get(self):
+#         result = []
+#         for so_line in self.sudo():
+#             name = '%s - %s' % (so_line.product_id.descripcion_cliente)
+#             # if so_line.order_partner_id.ref:
+#             #     name = '%s (%s)' % (name, so_line.order_partner_id.ref)
+#             result.append((so_line.id, name))
+#         return result
