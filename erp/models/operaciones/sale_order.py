74944609d2cc5812880e4e645405ae07672e0068
Alejandro <alejandro@agtrace.ag>
2020-08-24 00:04:13 -0300

    Creando el reporte de cotización

--- erp/models/operaciones/sale_order.py
+++ erp/models/operaciones/sale_order.py
@@ -18,9 +18,6 @@ class SaleOrderTemplate(models.Model):
     @api.depends('order_line.price_total')
     def _discount_total(self):
         self.discount_total = 0.00
-        self.amount_without_discount_total = 1
-
-        raise except_orm(self.amount_without_discount_total + 2)
 
         """
         Compute the amounts of the SO line.
@@ -41,7 +38,6 @@ class SaleOrderTemplate(models.Model):
     def _amount_without_discount_total(self):
         self.amount_without_discount_total = 0.00
 
-        raise except_orm(self.amount_without_discount_total + 2)
 
         """
         Compute the amounts of the SO line.
