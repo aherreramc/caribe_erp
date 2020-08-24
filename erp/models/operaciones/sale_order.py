56f3532218e4fd0b8f1fd5e1720d7b38bd00238a
Alejandro <alejandro@agtrace.ag>
2020-08-23 22:33:04 -0300

    Creando el reporte de cotización

--- erp/models/operaciones/sale_order.py
+++ erp/models/operaciones/sale_order.py
@@ -14,25 +14,6 @@ class SaleOrderTemplate(models.Model):
 
     discount_total = fields.Monetary(compute='_discount_total', string='Discount', store=True)
 
-    # @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
-    # def _compute_amount(self):
-    #
-    #
-    #     """
-    #     Compute the amounts of the SO line.
-    #     """
-    #     for line in self:
-    #         price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-    #         taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty,
-    #                                         product=line.product_id, partner=line.order_id.partner_id)
-    #
-    #         # self.discount_total += line.total_excluded - taxes['total_excluded']
-    #
-    #         line.update({
-    #             'price_tax': taxes['total_included'] - taxes['total_excluded'],
-    #             'price_total': taxes['total_included'],
-    #             'price_subtotal': taxes['total_excluded'],
-    #         })
 
     @api.depends('order_line.price_total')
     def _discount_total(self):
