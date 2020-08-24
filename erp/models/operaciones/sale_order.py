683ad7e054c2c2acd30a0a05d3ca09b6c1fb89d1
Alejandro <alejandro@agtrace.ag>
2020-08-23 21:41:00 -0300

    Creando el reporte de cotización

--- erp/models/operaciones/sale_order.py
+++ erp/models/operaciones/sale_order.py
@@ -2,10 +2,8 @@
 
 import odoo
 from odoo import api, fields, models, SUPERUSER_ID, tools, _
-
 import odoo.addons.decimal_precision as dp
 from openerp.exceptions import except_orm, Warning, RedirectWarning
-
 from openerp.exceptions import except_orm, Warning, RedirectWarning
 
 
@@ -16,17 +14,17 @@ class SaleOrderTemplate(models.Model):
 
     discount_total = fields.Monetary(compute='_compute_amount', string='Discount', readonly=True, store=True)
 
-
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
     def _compute_amount(self):
-        # self.discount_total = 0.00
+        self.discount_total = 0.00
 
         """
         Compute the amounts of the SO line.
         """
         for line in self:
             price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_id)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty,
+                                            product=line.product_id, partner=line.order_id.partner_id)
 
             # self.discount_total += line.total_excluded - taxes['total_excluded']
 
@@ -35,3 +33,5 @@ class SaleOrderTemplate(models.Model):
                 'price_total': taxes['total_included'],
                 'price_subtotal': taxes['total_excluded'],
             })
+
+    # def _discount_total(self):
