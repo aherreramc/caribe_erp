47b5c3d4df2adffe83e00e3f9c202fb40cb641d2
Alejandro <alejandro@agtrace.ag>
2020-08-27 17:43:13 -0300

    poniendo borde

--- erp/report_models/nomencladores/product_label_report.py
+++ erp/report_models/nomencladores/product_label_report.py
@@ -9,37 +9,7 @@ from openerp.exceptions import except_orm, Warning, RedirectWarning
 
 
 class ProductLabelReport(models.AbstractModel):
-    _name = 'report.erp.product_label_report'
-    
-    # @api.model
-    # def _get_report_values(self, docids, data=None):
-    #     self.model = self.env.context.get('active_model')
-    #     # product = self.env['product.template'].browse(self.env.context.get('active_id'))
-    #     #
-    #     # self.model = self.env.context.get('active_model')
-    #     # docs = self.env[self.model].browse(self.env.context.get('active_id'))
-    #
-    #     raise except_orm(docids)
-    #
-    #
-    #     report_obj = self.env['ir.actions.report']
-    #     report = report_obj._get_report_from_name('erp.product_label_report')
-    #
-    #     # raise except_orm(product.name)
-    #
-    #     docargs = {
-    #         'doc_ids': docids,
-    #         'doc_model': report.model,
-    #         'product': product,
-    #     }
-    #     return docargs
-    #
-    #     # docargs = {
-    #     #     'doc_ids': self.ids,
-    #     #     'doc_model': self.model,
-    #     #     'product' : product,
-    #     # }
-    #     # return self.env['report'].render('erp.product_label_report', docargs)
+    _name = 'report.erp.product_label_repo'
 
 
     @api.model
