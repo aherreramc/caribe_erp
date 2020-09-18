d0027b06898d50041683f60112aaabcde381914f
Alejandro <alejandro@agtrace.ag>
2020-09-17 23:18:41 -0300

    sale.order.line

--- erp/models/nomencladores/invoice_line.py
+++ erp/models/nomencladores/invoice_line.py
@@ -8,11 +8,11 @@ import odoo.addons.decimal_precision as dp
 from openerp.exceptions import except_orm, Warning, RedirectWarning
 
 
-class InvoiceLineTemplate(models.Model):
-    _inherit = 'invoice.line'
-
-    codigo_descripcion = fields.Char(string="Descripción", compute='_compute_codigo_descripcion')
-
-    @api.depends('product_id')
-    def _compute_codigo_descripcion(self):
-        self.name
+# class InvoiceLineTemplate(models.Model):
+#     _inherit = 'invoice.line'
+#
+#     codigo_descripcion = fields.Char(string="Descripción", compute='_compute_codigo_descripcion')
+#
+#     @api.depends('product_id')
+#     def _compute_codigo_descripcion(self):
+#         self.name
