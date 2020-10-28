b16ce5f10f07f59d0b3c06aaeb04cb883674ec52
Alejandro <alejandro@agtrace.ag>
2020-10-28 04:05:02 -0300

    label

--- erp/models/nomencladores/__init__.py
+++ erp/models/nomencladores/__init__.py
@@ -1,4 +1,20 @@
 # -*- coding: utf-8 -*-
 
-from . import *
+from . import product_template
+from . import tipo_de_espiga
+from . import potencia
+from . import tipo_de_producto
+from . import material
+from . import representante_cliente
+from . import marca
+from . import embalaje
+from . import puerto
+from . import dias_para_cobros_y_pagos
+from . import etapa
+from . import pago_nomenclador
+from . import garantia
+from . import firma
+from . import degradation_type
+from . import account_invoice_line
+from . import purchase_order
 
--- erp/models/operaciones/__init__.py
+++ erp/models/operaciones/__init__.py
@@ -1,1 +1,4 @@
-from . import *
+
+from . import sale_order_line
+from . import sale_order
+from . import price_list
