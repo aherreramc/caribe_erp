fad758b7fe1014262062c135db747a2c103739ca
Alejandro <alejandro@agtrace.ag>
2020-11-02 02:28:19 -0300

    label

--- erp/models/operaciones/price_list.py
+++ erp/models/operaciones/price_list.py
@@ -10,48 +10,48 @@ from openerp.exceptions import except_orm, Warning, RedirectWarning
 class PriceListTemplate(models.Model):
     _inherit = 'product.pricelist'
 
-    purchase_order = fields.Many2one('purchase.order', string='Purchase order')
-
-    @api.onchange('purchase_order')
-    def purchase_order_change(self):
-
-        for pricelist in self:
-            if pricelist.purchase_order is not False:
-
-                purchase_order = pricelist.purchase_order
-                # pricelist.item_ids.unlink();
-
-                new_lines = []
-
-                for purchase_line in purchase_order.order_line:
-                    raise except_orm(purchase_line.name)
-                    new_line = {
-                        'base': 'list_price',
-                        # 'applied_on': '1_product',
-                        # 'pricelist_id': pricelist.id,
-                        'product_tmpl_id': purchase_line.product_id.id,
-                        'price_discount': 0,
-                        'min_quantity': 0,
-                        # 'compute_price': 'fixed',
-                    }
-
-
-                    # self.env['product.pricelist.item'].create({
-                    #     'base': 'list_price',
-                    #     'applied_on': '1_product',
-                    #     'pricelist_id': pricelist.id,
-                    #     'product_tmpl_id': product_template.id,
-                    #     'price_discount': 20,
-                    #     'min_quantity': 2,
-                    #     'compute_price': 'formula',
-                    # })
-
-                    # new_lines.append(new_line)
-
-                    # self.item_ids.
-                #
-                #
-                # self.item_ids = new_lines
+    # purchase_order = fields.Many2one('purchase.order', string='Purchase order')
+    #
+    # @api.onchange('purchase_order')
+    # def purchase_order_change(self):
+    #
+    #     for pricelist in self:
+    #         if pricelist.purchase_order is not False:
+    #
+    #             purchase_order = pricelist.purchase_order
+    #             # pricelist.item_ids.unlink();
+    #
+    #             new_lines = []
+    #
+    #             for purchase_line in purchase_order.order_line:
+    #                 raise except_orm(purchase_line.name)
+    #                 new_line = {
+    #                     'base': 'list_price',
+    #                     # 'applied_on': '1_product',
+    #                     # 'pricelist_id': pricelist.id,
+    #                     'product_tmpl_id': purchase_line.product_id.id,
+    #                     'price_discount': 0,
+    #                     'min_quantity': 0,
+    #                     # 'compute_price': 'fixed',
+    #                 }
+    #
+    #
+    #                 # self.env['product.pricelist.item'].create({
+    #                 #     'base': 'list_price',
+    #                 #     'applied_on': '1_product',
+    #                 #     'pricelist_id': pricelist.id,
+    #                 #     'product_tmpl_id': product_template.id,
+    #                 #     'price_discount': 20,
+    #                 #     'min_quantity': 2,
+    #                 #     'compute_price': 'formula',
+    #                 # })
+    #
+    #                 # new_lines.append(new_line)
+    #
+    #                 # self.item_ids.
+    #             #
+    #             #
+    #             # self.item_ids = new_lines
 
 
 
@@ -62,6 +62,21 @@ class PriceListItemTemplate(models.Model):
     purchase_order_line = fields.Many2one('purchase.order.line', string='Order Line')
     purchase_price = fields.Float(string='Purchase price', digits=dp.get_precision('Product Price'), related='purchase_order_line.price_unit')
     # purchase_price = fields.Monetary(string='Purchase price', related='purchase_order_line.price_unit')
+
+    purchase_order = fields.Many2one('purchase.order', string='Purchase order')
+
+
+    base = fields.Selection([
+        ('purchase', 'Purchase Price'),
+        ('list_price', 'Sales Price'),
+        ('standard_price', 'Cost'),
+        ('pricelist', 'Other Pricelist')], "Based on",
+        default='purchase', required=True,
+        help='Base price for computation.\n'
+             'Purchase Price: The base price will be the Purchase Price.\n'
+             'Sales Price: The base price will be the Sales Price.\n'
+             'Cost Price : The base price will be the cost price.\n'
+             'Other Pricelist : Computation of the base price based on another Pricelist.')
 
 
 
