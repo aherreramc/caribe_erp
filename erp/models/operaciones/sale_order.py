c14e614eaf22172da26f30ca48203e54b1701018
Alejandro <alejandro@agtrace.ag>
2020-11-20 01:15:50 -0300

    label

--- erp/models/operaciones/sale_order.py
+++ erp/models/operaciones/sale_order.py
@@ -112,6 +112,15 @@ class SaleOrderLineTemplate(models.Model):
 
         if self.order_id.pricelist_id and self.order_id.partner_id:
             vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+
+        for price_list_item in self.order_id.pricelist_id.item_ids:
+            if price_list_item.base == 'purchase':
+                if price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id:
+                    # self.price_unit = price_list_item.total_margin
+                    self.price_unit = 7
+                    # self.update({'price_unit': 8})
+                    vals['price_unit'] = 9
+
         self.update(vals)
 
         title = False
@@ -129,13 +138,7 @@ class SaleOrderLineTemplate(models.Model):
 
 
 
-        for price_list_item in self.order_id.pricelist_id.item_ids:
 
-            if price_list_item.base == 'purchase':
-                if price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id:
-                    # self.price_unit = price_list_item.total_margin
-                    self.price_unit = 7
-                    self.update({'price_unit': 8})
 
 
 
