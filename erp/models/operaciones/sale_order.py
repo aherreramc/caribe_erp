7564e8f07ee533ae352041172b3d8023d986b16b
Alejandro <alejandro@agtrace.ag>
2020-11-19 22:51:59 -0300

    label

--- erp/models/operaciones/sale_order.py
+++ erp/models/operaciones/sale_order.py
@@ -77,18 +77,24 @@ class SaleOrderLineTemplate(models.Model):
 
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
     def _compute_amount(self):
-        raise except_orm("A")
         """
         Compute the amounts of the SO line.
         """
         for line in self:
             price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
             taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+            # line.update({
+            #     'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+            #     'price_total': taxes['total_included'],
+            #     'price_subtotal': taxes['total_excluded'],
+            # })
+
             line.update({
-                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-                'price_total': taxes['total_included'],
-                'price_subtotal': taxes['total_excluded'],
+                'price_tax': 1,
+                'price_total': 2,
+                'price_subtotal': 3,
             })
+
             if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
                 line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
 
