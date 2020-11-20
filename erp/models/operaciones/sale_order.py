304c0f5beac15ae7ad2454bdefac94ea0cf98f33
Alejandro <alejandro@agtrace.ag>
2020-11-20 01:04:59 -0300

    label

--- erp/models/operaciones/sale_order.py
+++ erp/models/operaciones/sale_order.py
@@ -132,8 +132,8 @@ class SaleOrderLineTemplate(models.Model):
         for price_list_item in self.order_id.pricelist_id.item_ids:
 
             if price_list_item.base == 'purchase':
-                raise except_orm(price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id)
                 if price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id:
+                    raise except_orm(price_list_item.total_margin)
                     self.price_unit = price_list_item.total_margin
 
 
