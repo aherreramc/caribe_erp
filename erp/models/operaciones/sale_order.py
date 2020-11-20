e3e6e258798e7ef37580ecdb8470e2133f4f6d9d
Alejandro <alejandro@agtrace.ag>
2020-11-20 11:32:59 -0300

    label

--- erp/models/operaciones/sale_order.py
+++ erp/models/operaciones/sale_order.py
@@ -117,32 +117,32 @@ class SaleOrderLineTemplate(models.Model):
             if price_list_item.base == 'purchase':
                 if price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id:
                     # self.update({'price_unit': price_list_item.total_margin})
-                    # self.price_unit = price_list_item.total_margin
+                    self.price_unit = price_list_item.total_margin
                     vals['price_unit'] = price_list_item.total_margin
 
 
-        self.update(vals)
+        # self.update(vals)
 
         # for price_list_item in self.order_id.pricelist_id.item_ids:
         #     if price_list_item.base == 'purchase':
         #         if price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id:
         #             self.update({'price_unit': price_list_item.total_margin})
 
-        title = False
-        message = False
-        result = {}
-        warning = {}
-        if product.sale_line_warn != 'no-message':
-            title = _("Warning for %s") % product.name
-            message = product.sale_line_warn_msg
-            warning['title'] = title
-            warning['message'] = message
-            result = {'warning': warning}
-            if product.sale_line_warn == 'block':
-                self.product_id = False
-
-
-        return result
+        # title = False
+        # message = False
+        # result = {}
+        # warning = {}
+        # if product.sale_line_warn != 'no-message':
+        #     title = _("Warning for %s") % product.name
+        #     message = product.sale_line_warn_msg
+        #     warning['title'] = title
+        #     warning['message'] = message
+        #     result = {'warning': warning}
+        #     if product.sale_line_warn == 'block':
+        #         self.product_id = False
+        #
+        #
+        # return result
 
 
 
