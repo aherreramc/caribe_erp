88cc92720a76df1149afebc555b3d938bc61b939
Alejandro <alejandro@agtrace.ag>
2020-11-25 12:11:37 -0300

    label

--- erp/models/operaciones/sale_order.py
+++ erp/models/operaciones/sale_order.py
@@ -127,73 +127,119 @@ class SaleOrderLineTemplate(models.Model):
     _inherit = 'sale.order.line'
 
 
+    @api.multi
     @api.onchange('product_id')
     def product_id_change(self):
         if not self.product_id:
-            return
-        valid_values = self.product_id.product_tmpl_id.valid_product_template_attribute_line_ids.product_template_value_ids
-        # remove the is_custom values that don't belong to this template
-        for pacv in self.product_custom_attribute_value_ids:
-            if pacv.custom_product_template_attribute_value_id not in valid_values:
-                self.product_custom_attribute_value_ids -= pacv
-
-        # remove the no_variant attributes that don't belong to this template
-        for ptav in self.product_no_variant_attribute_value_ids:
-            if ptav._origin not in valid_values:
-                self.product_no_variant_attribute_value_ids -= ptav
+            return {'domain': {'product_uom': []}}
 
         vals = {}
-        if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
+        domain = {'product_uom': [('category_id', '=', self.product_id.uom_id.category_id.id)]}
+        if not self.product_uom or (self.product_id.uom_id.category_id.id != self.product_uom.category_id.id):
             vals['product_uom'] = self.product_id.uom_id
-            vals['product_uom_qty'] = self.product_uom_qty or 1.0
 
         product = self.product_id.with_context(
-            lang=get_lang(self.env, self.order_id.partner_id.lang).code,
-            partner=self.order_id.partner_id,
-            quantity=vals.get('product_uom_qty') or self.product_uom_qty,
+            lang=self.order_id.partner_id.lang,
+            partner=self.order_id.partner_id.id,
+            quantity=self.product_uom_qty,
             date=self.order_id.date_order,
             pricelist=self.order_id.pricelist_id.id,
             uom=self.product_uom.id
         )
 
-        vals.update(name=self.get_sale_order_line_multiline_description_sale(product))
+        name = product.name_get()[0][1]
+        if product.description_sale:
+            name += '\n' + product.description_sale
+        vals['name'] = name
 
         self._compute_tax_id()
 
-        # if self.order_id.pricelist_id and self.order_id.partner_id:
-        #     vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+        if self.order_id.pricelist_id and self.order_id.partner_id:
+            vals['price_unit'] = self.env['account.tax']._fix_tax_included_price(self._get_display_price(product), product.taxes_id, self.tax_id)
+        self.update(vals)
 
-        for price_list_item in self.order_id.pricelist_id.item_ids:
-            if price_list_item.base == 'purchase':
-                if price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id:
-                    # self.update({'price_unit': price_list_item.total_margin})
-                    self.price_unit = price_list_item.total_margin
-                    # vals['price_unit'] = price_list_item.total_margin
-
-
-        # self.update(vals)
-
-        # for price_list_item in self.order_id.pricelist_id.item_ids:
-        #     if price_list_item.base == 'purchase':
-        #         if price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id:
-        #             self.update({'price_unit': price_list_item.total_margin})
-
-        # title = False
-        # message = False
-        result = {}
-        # warning = {}
-        # if product.sale_line_warn != 'no-message':
-        #     title = _("Warning for %s") % product.name
-        #     message = product.sale_line_warn_msg
-        #     warning['title'] = title
-        #     warning['message'] = message
-        #     result = {'warning': warning}
-        #     if product.sale_line_warn == 'block':
-        #         self.product_id = False
+        title = False
+        message = False
+        warning = {}
+        if product.sale_line_warn != 'no-message':
+            title = _("Warning for %s") % product.name
+            message = product.sale_line_warn_msg
+            warning['title'] = title
+            warning['message'] = message
+            if product.sale_line_warn == 'block':
+                self.product_id = False
+            return {'warning': warning}
+        return {'domain': domain}
 
 
-        return result
 
+    # @api.onchange('product_id')
+    # def product_id_change(self):
+    #     if not self.product_id:
+    #         return
+    #     valid_values = self.product_id.product_tmpl_id.valid_product_template_attribute_line_ids.product_template_value_ids
+    #     # remove the is_custom values that don't belong to this template
+    #     for pacv in self.product_custom_attribute_value_ids:
+    #         if pacv.custom_product_template_attribute_value_id not in valid_values:
+    #             self.product_custom_attribute_value_ids -= pacv
+    #
+    #     # remove the no_variant attributes that don't belong to this template
+    #     for ptav in self.product_no_variant_attribute_value_ids:
+    #         if ptav._origin not in valid_values:
+    #             self.product_no_variant_attribute_value_ids -= ptav
+    #
+    #     vals = {}
+    #     if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
+    #         vals['product_uom'] = self.product_id.uom_id
+    #         vals['product_uom_qty'] = self.product_uom_qty or 1.0
+    #
+    #     product = self.product_id.with_context(
+    #         lang=get_lang(self.env, self.order_id.partner_id.lang).code,
+    #         partner=self.order_id.partner_id,
+    #         quantity=vals.get('product_uom_qty') or self.product_uom_qty,
+    #         date=self.order_id.date_order,
+    #         pricelist=self.order_id.pricelist_id.id,
+    #         uom=self.product_uom.id
+    #     )
+    #
+    #     vals.update(name=self.get_sale_order_line_multiline_description_sale(product))
+    #
+    #     self._compute_tax_id()
+    #
+    #     # if self.order_id.pricelist_id and self.order_id.partner_id:
+    #     #     vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+    #
+    #     for price_list_item in self.order_id.pricelist_id.item_ids:
+    #         if price_list_item.base == 'purchase':
+    #             if price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id:
+    #                 # self.update({'price_unit': price_list_item.total_margin})
+    #                 self.price_unit = price_list_item.total_margin
+    #                 # vals['price_unit'] = price_list_item.total_margin
+    #
+    #
+    #     # self.update(vals)
+    #
+    #     # for price_list_item in self.order_id.pricelist_id.item_ids:
+    #     #     if price_list_item.base == 'purchase':
+    #     #         if price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id:
+    #     #             self.update({'price_unit': price_list_item.total_margin})
+    #
+    #     # title = False
+    #     # message = False
+    #     result = {}
+    #     # warning = {}
+    #     # if product.sale_line_warn != 'no-message':
+    #     #     title = _("Warning for %s") % product.name
+    #     #     message = product.sale_line_warn_msg
+    #     #     warning['title'] = title
+    #     #     warning['message'] = message
+    #     #     result = {'warning': warning}
+    #     #     if product.sale_line_warn == 'block':
+    #     #         self.product_id = False
+    #
+    #
+    #     return result
+    #
 
 
 
