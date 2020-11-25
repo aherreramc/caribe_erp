864b9f2a393ba7b5224d71c4845b6b374a9a959d
Alejandro <alejandro@agtrace.ag>
2020-11-25 11:36:25 -0300

    label

--- erp/models/operaciones/sale_order_line.py
+++ erp/models/operaciones/sale_order_line.py
@@ -17,8 +17,8 @@ class SaleOrderLineTemplate(models.Model):
     order_id = fields.Many2one('sale.order', string='Order Reference', required=True, ondelete='cascade', index=True, copy=False)
     item_currency_id = fields.Many2one('res.currency', 'Currency', related='order_id.currency_id')
 
-    sale_percent = fields.Float('Sale comision%', default=0, digits=(16, 2))
-    sale = fields.Monetary(string='Sale Comision', currency_field='item_currency_id', compute='_compute_part_prices', store=True)
+    sale_percent = fields.Float('Sale comision%', default=0, digits=(16, 2), compute='_compute_sale_comision')
+    sale = fields.Monetary(string='Sale Comision', currency_field='item_currency_id', compute='_compute_sale_comision', store=True)
 
     @api.onchange('product_id')
     def product_id_change(self):
@@ -26,3 +26,10 @@ class SaleOrderLineTemplate(models.Model):
         self.name = self.product_id.descripcion_cliente
 
         return result
+
+
+    @api.onchange('discount')
+    def _compute_sale_comision_on_change(self):
+        for order_line in self:
+            order_line.sale_percent += 1
+            order_line.sale_percent += 1
