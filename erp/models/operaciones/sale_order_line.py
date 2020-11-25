f0a11fb2db90d7f7884617020127f9ed8e3da9fd
Alejandro <alejandro@agtrace.ag>
2020-11-25 11:22:25 -0300

    label

--- erp/models/operaciones/sale_order_line.py
+++ erp/models/operaciones/sale_order_line.py
@@ -14,6 +14,12 @@ class SaleOrderLineTemplate(models.Model):
 
     discount_total = fields.Monetary(string='Discount', readonly=True, store=True)
 
+    order_id = fields.Many2one('sale.order', string='Order Reference', required=True, ondelete='cascade', index=True, copy=False)
+    item_currency_id = fields.Many2one('res.currency', 'Currency', related='order_id.currency_id')
+
+    sale_percent = fields.Float('Sale comision%', default=0, digits=(16, 2))
+    sale = fields.Monetary(string='Sale Comision', currency_field='item_currency_id', compute='_compute_part_prices', store=True)
+
     @api.onchange('product_id')
     def product_id_change(self):
         result = super(SaleOrderLineTemplate, self).product_id_change()
