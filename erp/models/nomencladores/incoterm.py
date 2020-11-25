70f93d8750738e003e8dde1c87e556a5e5b1c54b
Alejandro <alejandro@agtrace.ag>
2020-11-24 23:31:04 -0300

    label

--- erp/models/nomencladores/incoterm.py
+++ erp/models/nomencladores/incoterm.py
@@ -2,7 +2,6 @@
 
 from odoo import models, fields, api, tools, _
 import odoo.addons.decimal_precision as dp
-from odoo.addons.base.res.res_partner import WARNING_MESSAGE, WARNING_HELP
 
 from openerp.exceptions import except_orm, Warning, RedirectWarning
 
