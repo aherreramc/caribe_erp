4f2085050bfc045cd0d18d4b35d54abd2d965bf5
Alejandro <alejandro@agtrace.ag>
2020-11-26 11:18:16 -0300

    label

--- erp/models/operaciones/sale_order_line.py
+++ erp/models/operaciones/sale_order_line.py
@@ -103,10 +103,28 @@ class SaleOrderLineTemplate(models.Model):
     @api.depends('price_unit', 'discount')
     def _get_price_reduce(self):
         for line in self:
-            raise except_orm(line.price_list_item.id)
             if line.price_list_item.id is not False:
-                raise except_orm(line.price_list_item.sale_percent - line.discount)
                 line.sale_percent = line.price_list_item.sale_percent - line.discount
 
 
             line.price_reduce = line.price_unit * (1.0 - line.discount / 100.0)
+
+
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        """
+        for line in self:
+            if line.price_list_item.id is not False:
+                line.sale_percent = line.price_list_item.sale_percent - line.discount
+
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
+            if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
+                line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
