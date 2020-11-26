5e2d04415ec2be1076d33f5ce63f18220d90f8a2
Alejandro <alejandro@agtrace.ag>
2020-11-26 10:53:16 -0300

    label

--- erp/models/operaciones/sale_order_line.py
+++ erp/models/operaciones/sale_order_line.py
@@ -31,8 +31,80 @@ class SaleOrderLineTemplate(models.Model):
         return result
 
 
-    @api.onchange('discount')
-    def _compute_sale_comision_on_change(self):
-        for order_line in self:
-            order_line.sale_percent += 1
-            order_line.sale += 1
+
+    price_list_item = fields.Many2one('product.pricelist.item', string ="Price list item")
+
+
+    @api.onchange('product_id')
+    def product_id_change(self):
+        if not self.product_id:
+            return
+        valid_values = self.product_id.product_tmpl_id.valid_product_template_attribute_line_ids.product_template_value_ids
+        # remove the is_custom values that don't belong to this template
+        for pacv in self.product_custom_attribute_value_ids:
+            if pacv.custom_product_template_attribute_value_id not in valid_values:
+                self.product_custom_attribute_value_ids -= pacv
+
+        # remove the no_variant attributes that don't belong to this template
+        for ptav in self.product_no_variant_attribute_value_ids:
+            if ptav._origin not in valid_values:
+                self.product_no_variant_attribute_value_ids -= ptav
+
+        vals = {}
+        if not self.product_uom or (self.product_id.uom_id.id != self.product_uom.id):
+            vals['product_uom'] = self.product_id.uom_id
+            vals['product_uom_qty'] = self.product_uom_qty or 1.0
+
+        product = self.product_id.with_context(
+            lang=get_lang(self.env, self.order_id.partner_id.lang).code,
+            partner=self.order_id.partner_id,
+            quantity=vals.get('product_uom_qty') or self.product_uom_qty,
+            date=self.order_id.date_order,
+            pricelist=self.order_id.pricelist_id.id,
+            uom=self.product_uom.id
+        )
+
+
+
+
+        vals.update(name=self.get_sale_order_line_multiline_description_sale(product))
+
+        self._compute_tax_id()
+
+        if self.order_id.pricelist_id and self.order_id.partner_id:
+            vals['price_unit'] = self.env['account.tax']._fix_tax_included_price_company(self._get_display_price(product), product.taxes_id, self.tax_id, self.company_id)
+
+        for price_list_item in self.order_id.pricelist_id.item_ids:
+            if price_list_item.base == 'purchase':
+                if price_list_item.product_tmpl_id.id == self.product_id.product_tmpl_id.id:
+                    vals['price_unit'] = price_list_item.total_margin
+                    vals['price_list_item'] = price_list_item.id
+
+
+
+        self.update(vals)
+
+        title = False
+        message = False
+        result = {}
+        warning = {}
+        if product.sale_line_warn != 'no-message':
+            title = _("Warning for %s") % product.name
+            message = product.sale_line_warn_msg
+            warning['title'] = title
+            warning['message'] = message
+            result = {'warning': warning}
+            if product.sale_line_warn == 'block':
+                self.product_id = False
+
+        return result
+
+
+    @api.depends('price_unit', 'discount')
+    def _get_price_reduce(self):
+        for line in self:
+            if line.price_list_item.id is not False:
+                line.sale_percent = line.price_list_item.sale_percent - line.discount
+
+
+            line.price_reduce = line.price_unit * (1.0 - line.discount / 100.0)
