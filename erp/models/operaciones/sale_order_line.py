6dbeb29529e3fa8a26b9adc05c7c2cf3c826cb13
Alejandro <alejandro@agtrace.ag>
2020-11-27 14:49:40 -0300

    label

--- erp/models/operaciones/sale_order_line.py
+++ erp/models/operaciones/sale_order_line.py
@@ -108,7 +108,8 @@ class SaleOrderLineTemplate(models.Model):
             if line.price_list_item.id is not False:
                 if line.sale_percent != 100:
                     line.sale_percent = line.price_list_item.sale_percent - line.discount
-                    line.sale = price / (1 - line.sale_percent / 100)
+                    price_before_sale_comision = line.price_list_item.price_before_sale_comision()
+                    line.sale = (price_before_sale_comision / (1 - line.sale_percent / 100)) - price_before_sale_comision
 
 
     @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
