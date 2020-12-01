41eb056e11ee93eb96e364287d0792480623b04a
Alejandro <alejandro@agtrace.ag>
2020-12-01 11:14:10 -0300

    label

---
+++ erp/models/operaciones/account_move_line.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import odoo
+from odoo import api, fields, models, SUPERUSER_ID, tools, _
+
+import odoo.addons.decimal_precision as dp
+from openerp.exceptions import except_orm, Warning, RedirectWarning
+from odoo.tools.misc import formatLang, get_lang
+
+
+
+class AccountMoveLineTemplate(models.Model):
+    _inherit = 'account.move.line'
+
+    sale_percent = fields.Float('Sale comision%', default=0, digits=(16, 2), store=True)
+    sale = fields.Monetary(string='Sale Comision', currency_field='item_currency_id', store=True)
