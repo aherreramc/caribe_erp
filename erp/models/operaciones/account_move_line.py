4f66ecc82689e95392b3f3bdaf31b75cf520d545
Alejandro <alejandro@agtrace.ag>
2020-12-01 11:18:54 -0300

    label

--- erp/models/operaciones/account_move_line.py
+++ erp/models/operaciones/account_move_line.py
@@ -12,5 +12,10 @@ from odoo.tools.misc import formatLang, get_lang
 class AccountMoveLineTemplate(models.Model):
     _inherit = 'account.move.line'
 
+    account_id = fields.Many2one('account.account', string='Account', index=True, ondelete="restrict", check_company=True,
+        domain=[('deprecated', '=', False)])
+
+    item_currency_id = fields.Many2one('res.currency', 'Currency', related='account_id.currency_id')
+
     sale_percent = fields.Float('Sale comision%', default=0, digits=(16, 2), store=True)
     sale = fields.Monetary(string='Sale Comision', currency_field='item_currency_id', store=True)
