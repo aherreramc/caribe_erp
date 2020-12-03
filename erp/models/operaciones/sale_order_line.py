7655e5a08207a2a0182b5176bb82983654ce53f8
Alejandro <alejandro@agtrace.ag>
2020-12-03 10:19:51 -0300

    label

--- erp/models/operaciones/sale_order_line.py
+++ erp/models/operaciones/sale_order_line.py
@@ -117,34 +117,34 @@ class SaleOrderLineTemplate(models.Model):
                     line.sale = (price_before_sale_comision / (1 - line.sale_percent / 100)) - price_before_sale_comision
 
 
-    # @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
-    # def _compute_amount(self):
-    #     """
-    #     Compute the amounts of the SO line.
-    #     """
-    #     for line in self:
-    #
-    #
-    #         price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
-    #         taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
-    #         line.update({
-    #             'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
-    #             'price_total': taxes['total_included'],
-    #             'price_subtotal': taxes['total_excluded'],
-    #         })
-    #         if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
-    #             line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
-    #
-    #
-    #         if line.price_list_item.id is not False:
-    #             if line.price_list_item.base == 'purchase':
-    #
-    #                 line.price_unit = line.price_list_item.total_margin
-    #
-    #                 if line.sale_percent != 100:
-    #                     line.sale_percent = line.price_list_item.sale_percent - line.discount
-    #                     price_before_sale_comision = line.price_list_item.price_before_sale_comision()
-    #                     line.sale = (price_before_sale_comision / (1 - line.sale_percent / 100)) - price_before_sale_comision
+    @api.depends('product_uom_qty', 'discount', 'price_unit', 'tax_id')
+    def _compute_amount(self):
+        """
+        Compute the amounts of the SO line.
+        """
+        for line in self:
+
+
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = line.tax_id.compute_all(price, line.order_id.currency_id, line.product_uom_qty, product=line.product_id, partner=line.order_id.partner_shipping_id)
+            line.update({
+                'price_tax': sum(t.get('amount', 0.0) for t in taxes.get('taxes', [])),
+                'price_total': taxes['total_included'],
+                'price_subtotal': taxes['total_excluded'],
+            })
+            if self.env.context.get('import_file', False) and not self.env.user.user_has_groups('account.group_account_manager'):
+                line.tax_id.invalidate_cache(['invoice_repartition_line_ids'], [line.tax_id.id])
+
+
+            if line.price_list_item.id is not False:
+                if line.price_list_item.base == 'purchase':
+
+                    line.price_unit = line.price_list_item.total_margin
+
+                    if line.sale_percent != 100:
+                        line.sale_percent = line.price_list_item.sale_percent - line.discount
+                        price_before_sale_comision = line.price_list_item.price_before_sale_comision()
+                        line.sale = (price_before_sale_comision / (1 - line.sale_percent / 100)) - price_before_sale_comision
 
 
 
