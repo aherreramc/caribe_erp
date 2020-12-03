4e378d430717cfba9a6eb20d905466c9f9d1474c
Alejandro <alejandro@agtrace.ag>
2020-12-03 10:52:34 -0300

    label

--- erp/models/operaciones/sale_order_line.py
+++ erp/models/operaciones/sale_order_line.py
@@ -138,8 +138,6 @@ class SaleOrderLineTemplate(models.Model):
             if line.price_list_item.id is not False:
                 if line.price_list_item.base == 'purchase':
 
-                    line.price_unit = line.price_list_item.total_margin
-
                     if line.sale_percent != 100:
                         line.sale_percent = line.price_list_item.sale_percent - line.discount
                         price_before_sale_comision = line.price_list_item.price_before_sale_comision()
