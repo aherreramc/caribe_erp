bcb701903f42c12986829e7d5b38120bd93b0271
Alejandro <alejandro@agtrace.ag>
2021-01-09 14:28:31 -0300

    label

--- erp/models/operaciones/sale_order.py
+++ erp/models/operaciones/sale_order.py
@@ -63,7 +63,7 @@ class SaleOrderTemplate(models.Model):
     marcas_encabezado = fields.Char(default="Por este medio, le comunicamos nuestra mejor oferta de productos de marca ")
     marcas = fields.Many2many('erp.nomencladores.marca', 'erp_operaciones_oferta_marcas', 'oferta_id', 'marca_id', 'Marcas')
 
-    cliente = fields.Many2one('res.partner', domain="[('usage', '=', 'customer')]")
+    proveedor = fields.Many2one('res.partner')
 
     # ('usage', '=', 'customer')
     # incoterm = fields.Many2one('erp.nomencladores.incoterm')
