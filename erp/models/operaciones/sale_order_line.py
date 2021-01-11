1648bb03bdb0e6aa88ef92cdce5653e3cd657f47
Alejandro <alejandro@agtrace.ag>
2021-01-11 08:24:03 -0300

    label

--- erp/models/operaciones/sale_order_line.py
+++ erp/models/operaciones/sale_order_line.py
@@ -161,8 +161,10 @@ class SaleOrderLineTemplate(models.Model):
                         line.sale_percent = line.price_list_item.sale_percent - line.discount
 
                         price_before_sale_comision = line.price_list_item.price_before_sale_comision()
-                        line.sale = ((price_before_sale_comision / (1 - line.sale_percent / 100)) - price_before_sale_comision) \
-                                        * line.product_uom_qty
+                        # line.sale = ((price_before_sale_comision / (1 - line.sale_percent / 100)) - price_before_sale_comision) \
+                        #                 * line.product_uom_qty
+
+                        line.sale = ((price_before_sale_comision - line.price_subtotal)) * line.product_uom_qty
 
 
 
